8a3fe0a0c2a92439335f7491174b3ccab39673f7
fernandocerveira <fernandocerveira@192.168.100.10>
2017-11-17 08:20:18 -0200

    Criada classe Empregado

---
+++ src/main/java/model/Empregado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author fernandocerveira
+ */
+public class Empregado {
+    int matricula;
+    String nome;
+    Boolean livre;
+    
+    
+}
