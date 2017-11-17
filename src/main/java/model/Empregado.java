0f369e29aa8c523bafbf4f6a6f1994f5134754ec
fernandocerveira <fernandocerveira@192.168.100.10>
2017-11-17 08:50:08 -0200

    Declarada a lista de funcoes.

--- src/main/java/model/Empregado.java
+++ src/main/java/model/Empregado.java
@@ -6,6 +6,7 @@
 package model;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,7 @@ public class Empregado implements Serializable {
     int matricula;
     String nomeEmpregado;
     Boolean livre;
+    List<Funcoes> funcao;
     
     private static final long serialVersionUID = 1L;
     @Id
