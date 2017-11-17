e81fff450c4d7ee8339ef538fe5f1ee85dbb2a30
fernandocerveira <fernandocerveira@192.168.100.10>
2017-11-17 08:22:19 -0200

    Deletada classe Empregado e criada Entidade empregado.

--- src/main/java/model/Empregado.java
+++ src/main/java/model/Empregado.java
@@ -5,14 +5,59 @@
  */
 package model;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author fernandocerveira
  */
-public class Empregado {
+@Entity
+public class Empregado implements Serializable {
+    
     int matricula;
-    String nome;
+    String nomeEmpregado;
     Boolean livre;
     
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Empregado)) {
+            return false;
+        }
+        Empregado other = (Empregado) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Empregado[ id=" + id + " ]";
+    }
     
 }
